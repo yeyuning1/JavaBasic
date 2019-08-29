f60666c9ffd279f0ccd414cf9aac091c9b6711ea
yeyuning1 <yeyuning1@gmail.com>
2019-08-29 21:28:16 +0800

    'OOP'

--- first/src/OOP/methods.java
+++ first/src/OOP/methods.java
@@ -1,4 +1,57 @@
 package OOP;
 
 public class methods {
+    public static void main(String[] args) {
+        Person ming = new Person();
+        ming.setAge(20);
+        ming.setName("小明");
+        int age = ming.getAge();
+        String name = ming.getName();
+        System.out.printf("%s 的年纪为 %d", name, age);
+    }
+}
+
+class Person {
+    private String name;
+    private int age;
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("invalid name");
+        }
+        this.name = name.strip(); // 去掉首尾空格
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public void setAge(int age) {
+        if (age < 0 || age > 100) {
+            throw new IllegalArgumentException("invalid age value");
+        }
+        this.age = age;
+    }
+}
+
+class Person1 {
+    private String name;
+    private int birth;
+
+    public void setBirth(int birth) {
+        this.birth = birth;
+    }
+
+    public int getAge() {
+        return calcAge(2019); // 调用private方法
+    }
+
+    // private方法:
+    private int calcAge(int currentYear) {
+        return currentYear - this.birth;
+    }
 }
